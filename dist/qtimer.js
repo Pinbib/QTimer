64176b011528cb427dae71bdbe0c06150837b7d8
Pinbib <pancenkoe353@gmail.com>
2024-04-20 12:56:12 +0100

    added mode --count

--- dist/qtimer.js
+++ dist/qtimer.js
@@ -22,12 +22,26 @@ var QTIMER = `\n░██████╗░████████╗██╗�
 
 console.log(colorize.gi(QTIMER));
 
+let mode = undefined;
 let type = undefined;
+let config = {};
 let command = "";
 
 if (process.argv[2] === "--log") {
-    type = "log";
+    mode = "log";
     command = process.argv.slice(3).join(" ");
+} else if (process.argv[2] === "--count") {
+    try {
+        type = "count";
+        config.count = Number(process.argv[3]);
+
+        command = process.argv.slice(4).join(" ");
+    } catch (err) {
+        if (err) {
+            console.log(colorize.rb("An error occurred."));
+            type = undefined;
+        }
+    }
 } else {
     command = process.argv.slice(2).join(" ");
 }
@@ -36,43 +50,100 @@ function formatTime(time) {
     return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`;
 }
 
+function convertTimeUnit(milliseconds) {
+    if (milliseconds < 0 || isNaN(milliseconds)) {
+        return "NaN";
+    }
+
+    if (milliseconds === 0) {
+        return "0ml";
+    }
+
+    const coefficients = {
+        h: 1000 * 60 * 60,
+        m: 1000 * 60,
+        s: 1000
+    };
+
+    const unit = Object.keys(coefficients).find(unit => milliseconds >= coefficients[unit]);
+
+    const amount = Math.floor(milliseconds / coefficients[unit]);
+
+    return `${amount}${unit}`;
+}
+
 try {
-    function spawn(command, callback) {
-        let com = node_child_process.exec(command);
-        let info = {};
+    function spawn(command) {
+        return new Promise((resolve, reject) => {
+            let com = node_child_process.exec(command);
+            let info = {};
 
-        com.on("spawn", () => {
-            info.start = new Date();
+            com.on("spawn", () => {
+                info.start = new Date();
+            });
+
+            com.stderr.on("error", (err) => {
+                reject(new Error("An error occurred."));
+            });
+
+            com.on("exit", (code) => {
+                info.end = new Date();
+                info.exitCode = colorize.yi(code);
+                info.time = info.end - info.start;
+
+                info.start = colorize.yi(formatTime(info.start));
+                info.end = colorize.yi(formatTime(info.end));
+
+                resolve(info);
+            });
+
+            if (mode === "log") {
+                com.stderr.on("data", console.log);
+                com.stdout.on("data", console.log);
+
+                // TODO: add input capability
+            }
         });
+    };
 
-        com.stderr.on("error", (err) => {
-            throw new Error("An error occurred.");
+    if (!type) {
+        spawn(command).then((info) => {
+            info.time = colorize.yb(info.time + "ml");
+
+            consoleTablePrinter.printTable([info]);
+        }).catch((err) => {
+            throw err;
         });
+    } else if (type === "count") {
+        (async () => {
+            let info = {
+                start: new Date()
+            };
 
-        com.on("exit", (code) => {
+            let infs = [];
+
+            for (let i = 0; i < config.count; i++) {
+                infs.push(await spawn(command));
+            }
+
             info.end = new Date();
-            info.exitCode = colorize.yi(code);
-            info.time = info.end - info.start;
+
+            info.full = colorize.yb(convertTimeUnit(info.end - info.start));
+
+            let average = 0;
+
+            infs.forEach(val => {
+                average += val.time;
+            });
+
+            info.average = colorize.yb(Math.round(average / infs.length) + "ml");
 
             info.start = colorize.yi(formatTime(info.start));
             info.end = colorize.yi(formatTime(info.end));
 
-            callback(info);
-        });
-
-        if (type === "log") {
-            com.stderr.on("data", console.log);
-            com.stdout.on("data", console.log);
-
-            // TODO: add input capability
-        }
-    };
-
-    spawn(command, (info) => {
-        info.time = colorize.yb(info.time + "ml");
-
-        consoleTablePrinter.printTable([info]);
-    });
+            consoleTablePrinter.printTable([info]);
+        })();
+    }
 } catch (err) {
     if (err) {
         console.log(colorize.rb("An error occurred."));
